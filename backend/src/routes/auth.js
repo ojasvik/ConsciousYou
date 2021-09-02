1e822ff7c2110d5c59043319c44df89c08e2378e
ojasvik <khuranaojasvi@gmail.com>
2021-09-02 21:11:53 +0530

    added validators

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { signup,signin, requireSignIn } = require('../controllers/auth');
+const { signup, signin } = require('../controllers/auth');
+const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require('../validators/auth');
 
+router.post('/signup', validateSignupRequest, isRequestValidated, signup);
+router.post('/signin', validateSigninRequest, isRequestValidated, signin);
 
-
-router.post('/signup', signup);
-
-router.post('/signin', signin);
-
-router.post('/profile',requireSignIn,(req,res) => {
-    res.status(200).json({message: 'User profile'});
-});
+// router.post('/profile',requireSignIn,(req,res) => {
+//     res.status(200).json({message: 'User profile'});
+// });
 
 module.exports = router;
---
+++ backend/src/validators/auth.js
@@ -0,0 +1,30 @@
+const { check, validationResult } = require("express-validator");
+
+exports.validateSignupRequest = [
+    check('firstName')
+        .notEmpty()
+        .withMessage('firstName is required'),
+    check('lastName')
+        .notEmpty()
+        .withMessage('lastName is required'),
+    check('email')
+        .isEmail()
+        .withMessage('Valid Email is required'),
+    check('password')
+        .isLength({ min: 8 })
+        .withMessage('Password must be atleast 8 characters long')
+];
+
+exports.validateSigninRequest = [
+    check('email')
+        .isEmail()
+        .withMessage('Valid Email is required'),
+];
+
+exports.isRequestValidated = (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.array().length > 0) {
+        return res.status(400).json({ error: errors.array()[0].msg })
+    }
+    next();
+};
